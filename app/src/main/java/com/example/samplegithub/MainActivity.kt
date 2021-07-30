3f0d1c9168afbd5a8b22998d1b7d3daa45501a11
Ingrid Silva <ingridsilvatec@outlook.com>
2021-07-30 20:06:43 -0300

    WIP: refatora layout, cria arquivo de dimens, cria top bar menu

--- app/src/main/java/com/example/samplegithub/MainActivity.kt
+++ app/src/main/java/com/example/samplegithub/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.samplegithub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.samplegithub.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        layoutInflate()
+        setContentView(binding.root)
     }
+
+    private fun layoutInflate() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+    }
+
 }
